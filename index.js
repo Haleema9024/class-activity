c2c84e26495b76e66bf925ebf656b4337721f447
Haleema Sadia <157414286+Haleema9024@users.noreply.github.com>
2024-05-15 16:07:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// question number:1
+
+let person_age = +prompt("Enter your Age");
+if(person_age >= 18){
+    ("You are eligible to vote.")
+}
+else{
+    console.log("You are not eligible to vote yet.")
+}
+
+
+// question number:2
+
+let temperature = +prompt ('Please enter the temperature in Celsius: ')
+
+
+if (temperature > 25)
+    {console.log("It's a hot day!")}
+else{
+    console.log("It's a normal day.")
+}
+
+
+
+
+// question number:3
+
+let student_grade = +prompt("Enter your score");
+if(student_grade >= 60){
+    ("You passed the test!")
+}
+else{
+    console.log("You failed the test.")
+}
+
+
+
+// question number:4
+
+let raining = true;
+if(raining == true){
+    console.log("remember to bring an umbrella");
+}
+else{
+    console.log("Enjoy the sunny weather")
+}
+
+
+
+// question number:5
+
+let person_age = +prompt('Please enter your age: ')
+
+if (person_age >= 18)
+   {
+    console.log("You are an adult.")
+   }
+else{ 
+    console.log("You are a minor.")
+}
+
+
+
+
+// question number:6
+
+let number = +prompt("Enter the number");
+if(number > 0){
+    console.log("the number is positive");
+}
+else if(number < 0){
+    console.log("the number is negative");
+}
+else if(number = 0)
+   { console.log("the number is zero");
+}
+else{
+    console.log("all number are integar");
+}
+
